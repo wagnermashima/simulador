387f6f21519b41ac409a2705993bd905a127433b
wagnermashima <wagnermashima@gmail.com>
2020-06-03 21:07:14 -0300

    first commit

---
+++ src/main/java/br/feevale/simulador/domain/Chamado.java
@@ -0,0 +1,15 @@
+package br.feevale.simulador.domain;
+
+import java.util.Random;
+
+import org.apache.commons.math3.analysis.function.Gaussian;
+import org.apache.commons.math3.random.UniformRandomGenerator;
+
+public class Chamado {
+
+	public static void main(String[] args) {
+		new UniformRandomGenerator(generator)
+		
+		new normal
+	}
+}
